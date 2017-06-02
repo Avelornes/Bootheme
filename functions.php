e2f3aaeb0279f1c68aef9a52fcd20fb405c32cc0
Avelornes <severine.noel94@gmail.com>
2017-06-02 22:16:37 +0200

    Initial commit

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+
+function theme_styles() {
+
+    wp_enqueue_style( 'bootstrap_css', get_template_directory_uri() . '/assets/librairies/bootstrap/css/bootstrap.css' );
+    wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css' );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_styles');
+
+function theme_js() {
+
+    global $wp_scripts;
+
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/assets/librairies/jquery-3.2.1.js');
+    wp_enqueue_script( 'bootstrap_js', get_template_directory_uri() . '/assets/librairies/bootstrap/js/bootstrap.min.js' );
+    //wp_enqueue_script( 'my_custom_js', get_template_directory_uri() . '/js/scripts.js');
+
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_js');
+
+?>
