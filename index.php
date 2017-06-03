b6c0f9ea62bb10aabf3c9f02a5aad5d0462f6690
Avelornes <severine.noel94@gmail.com>
2017-06-03 19:27:05 +0200

    add the loop for posts

--- index.php
+++ index.php
@@ -1,7 +1,33 @@
-<?php
+<?php get_header(); ?>
 
-get_header(); ?>
+<!-- Start the Loop. -->
+<?php if (have_posts()) : while (have_posts()) :
+    the_post(); ?>
 
+    <?php if (in_category('3')) : ?>
+    <div class="post-cat-three"></div>
+<?php else : ?>
+    <div class="post">
+<?php endif; ?>
+
+    <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
+           title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+    <small><?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></small>
+
+    <div class="entry">
+        <?php the_content(); ?>
+    </div>
+
+    <p class="postmetadata"><?php _e('Posted in'); ?><?php the_category(', '); ?></p>
+    </div> <!-- closes the first div box -->
+
+<?php endwhile;
+else : ?>
+
+    <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+
+<?php endif; ?>
 
 <?php wp_footer(); ?>
 </body>
