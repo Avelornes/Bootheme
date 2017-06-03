1874bb8f742c8fbbc7d07c38727fc8b00c225706
Avelornes <severine.noel94@gmail.com>
2017-06-03 23:18:13 +0200

    maj info

--- index.php
+++ index.php
@@ -20,10 +20,10 @@ the_post(); ?>
     <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
            title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-    <small><?php echo get_the_date(); ?> - <?php the_author_posts_link(); ?></small>
+    <small><?php echo get_the_date(); ?> par <?php the_author_posts_link(); ?> <?php _e('Posté dans '); ?><?php the_category(', '); ?>
 
     <div class="entry">
-        <?php the_content(); ?>
+        <?php the_excerpt(); ?>
         <?php get_attached_media($type, $post_id) ?>
     </div>
 
