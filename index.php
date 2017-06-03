576c137af3e2f9aa6add54e250e80e8bbb182c84
Avelornes <severine.noel94@gmail.com>
2017-06-03 21:13:47 +0200

    add media in posts

--- index.php
+++ index.php
@@ -9,14 +9,15 @@
 <?php else : ?>
     <div class="post">
 <?php endif; ?>
-
+    <?php the_post_thumbnail('large') ?>
     <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
            title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 
-    <small><?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></small>
+    <small><?php echo get_the_date(); ?> - <?php the_author_posts_link(); ?></small>
 
     <div class="entry">
         <?php the_content(); ?>
+        <?php get_attached_media( $type, $post_id ) ?>
     </div>
 
     <p class="postmetadata"><?php _e('Posted in'); ?><?php the_category(', '); ?></p>
