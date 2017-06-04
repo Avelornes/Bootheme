62809e1374ae29ed05307ef00672b3e5c9ce7c94
Avelornes <severine.noel94@gmail.com>
2017-06-04 12:58:01 +0200

    add template-parts content-excerpt

--- category.php
+++ category.php
@@ -12,23 +12,15 @@
 <?php endif; ?>
 
 	<?php
-	if ( $thumbnail_html = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ) ):
-		$thumbnail_src = $thumbnail_html['0'];
-		?>
-        <img class="img-responsive img-thumbnail" src="<?php echo $thumbnail_src ?>" alt="">
-	<?php endif; ?>
 
-    <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
-           title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
-    <small><?php echo get_the_date(); ?>
-        par <?php the_author_posts_link(); ?> <?php _e( 'Posté dans ' ); ?><?php the_category( ', ' ); ?></small>
-
-    <div class="entry">
-		<?php the_excerpt(); ?>
-		<?php get_attached_media( $type, $post_id ) ?>
+	$post_display_option = get_theme_mod( 'post_display_option', 'post-excerpt' );
+	if ( 'post-excerpt' === $post_display_option ) {
+		get_template_part( 'template-parts/content','excerpt' );
+	} else {
+		get_template_part( 'template-parts/content', get_post_format() );
+	}
+	?>
     </div>
-    </div> <!-- closes the first div box -->
 
 <?php endwhile;
 else : ?>
