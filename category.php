0b614ea3a0e432ed06945395527340212b49f885
Avelornes <severine.noel94@gmail.com>
2017-06-04 12:53:41 +0200

    create category

---
+++ category.php
@@ -0,0 +1,41 @@
+<?php get_header(); ?>
+<div><strong><?php single_cat_title( 'Archives de ', 'true' ); ?>
+    </strong>: <?php echo category_description(); ?></div>
+<!-- Start the Loop. -->
+<?php if ( have_posts() ) : while ( have_posts() ) :
+	the_post(); ?>
+
+	<?php if ( in_category( '3' ) ) : ?>
+    <div class="post-cat-three"></div>
+<?php else : ?>
+    <div class="post">
+<?php endif; ?>
+
+	<?php
+	if ( $thumbnail_html = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ) ):
+		$thumbnail_src = $thumbnail_html['0'];
+		?>
+        <img class="img-responsive img-thumbnail" src="<?php echo $thumbnail_src ?>" alt="">
+	<?php endif; ?>
+
+    <h2><a href="<?php the_permalink(); ?>" rel="bookmark"
+           title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+    <small><?php echo get_the_date(); ?>
+        par <?php the_author_posts_link(); ?> <?php _e( 'Posté dans ' ); ?><?php the_category( ', ' ); ?></small>
+
+    <div class="entry">
+		<?php the_excerpt(); ?>
+		<?php get_attached_media( $type, $post_id ) ?>
+    </div>
+    </div> <!-- closes the first div box -->
+
+<?php endwhile;
+else : ?>
+
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
+
