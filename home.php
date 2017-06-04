02aa831f7e5febe4f044a7eb8912473ca9675f63
Avelornes <severine.noel94@gmail.com>
2017-06-04 19:17:35 +0200

    create homepage for blog section

--- home.php
+++ home.php
@@ -1,15 +1,14 @@
 <?php get_header(); ?>
 
-<!-- Start the Loop. -->
+	<!-- Start the Loop. -->
 <?php if ( have_posts() ) : while ( have_posts() ) :
 	the_post(); ?>
 
 	<?php if ( in_category( '3' ) ) : ?>
-    <div class="post-cat-three"></div>
+	<div class="post-cat-three"></div>
 <?php else : ?>
-    <div class="post">
+	<div class="post">
 <?php endif; ?>
-
 	<?php
 
 	$post_display_option = get_theme_mod( 'post_display_option', 'post-excerpt' );
@@ -19,14 +18,13 @@
 		get_template_part( 'template-parts/content', get_post_format() );
 	}
 	?>
-    </div>
+	</div>
 
 <?php endwhile;
 else : ?>
 
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
 
 <?php endif; ?>
 
 <?php get_footer(); ?>
-
