aff79dcb972eebdacdd219815e5209ae6fa426b3
Avelornes <severine.noel94@gmail.com>
2017-06-05 17:19:37 +0200

    create content-page, no excerpt

---
+++ template-parts/content-page.php
@@ -0,0 +1,28 @@
+<?php if (have_posts() ) : while ( have_posts() ) :
+	the_post(); ?>
+
+	<?php if ( in_category( '3' ) ) : ?>
+	<div class="post-cat-three"></div>
+<?php else : ?>
+	<div class="post">
+<?php endif; ?>
+	<?php
+	if ( $thumbnail_html = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'large' ) ):
+		$thumbnail_src = $thumbnail_html['0'];
+		?>
+		<img class="img-responsive img-thumbnail" src="<?php echo $thumbnail_src ?>" alt=""/>
+	<?php endif; ?>
+
+	<h2><?php the_title(); ?></h2>
+
+	<div class="entry">
+		<?php the_content(); ?>
+		<?php get_attached_media( $type, $post_id ) ?>
+	</div>
+	</div>
+<?php endwhile;
+else : ?>
+
+	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+
+<?php endif; ?>
