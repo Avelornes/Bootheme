026d2a83a7d2eef4ccb5794a5f6d421b69733d09
Avelornes <severine.noel94@gmail.com>
2017-06-13 22:32:19 +0200

    build and save option page form reindent

--- includes/save-options-page.php
+++ includes/save-options-page.php
@@ -1,16 +1,16 @@
 <?php
 
 function boot_save_options() {
-	if(!current_user_can('publish_pages')){
-		wp_die('Vous n\'êtes pa autorisé à effectuer cette opération');
+	if ( ! current_user_can( 'publish_pages' ) ) {
+		wp_die( 'Vous n\'êtes pa autorisé à effectuer cette opération' );
 	}
-	check_admin_referer('boot_options_verify');
-	$opts = get_option('boot_opts');
-	$opts['legend_01'] = sanitize_text_field($POST["boot_legend_01"]) ;
+	check_admin_referer( 'boot_options_verify' );
+	$opts              = get_option( 'boot_opts' );
+	$opts['legend_01'] = sanitize_text_field( $POST["boot_legend_01"] );
 
-	update_option('boot_opts', $opts);
+	update_option( 'boot_opts', $opts );
 
-	wp_redirect(admin_url('admin.php?page=boot_theme_opts&status=1'));
+	wp_redirect( admin_url( 'admin.php?page=boot_theme_opts&status=1' ) );
 	exit;
 }
 
