83ab1407b314a98daab52c216b3662c522c43dcc
Avelornes <severine.noel94@gmail.com>
2017-06-18 22:37:40 +0200

    admin options (but not necessary)

---
+++ assets/js/admin-options.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Séverine on 18/06/17.
+ */
+
+jQuery(document).ready(function ($) {
+
+    /*add image with build option page*/
+    var frame = wp.media({
+        title: 'Sélectionner une image',
+        button: {
+            text: 'Utiliser ce média'
+        },
+        multiple: false
+    });
+
+    $('#form-boot-options #btn_img_01').click(function (e) {
+        e.preventDefault();
+        frame.open();
+    });
+
+    frame.on('select', function () {
+        var objImg = frame.state().get('selection').first().toJSON();
+        var mon_url = objImg.sizes.medium_large.url;
+        $('#img#img_preview_01').attr('src', mon_url);
+        $('input#boot_image_01').attr('value', mon_url);
+    });
+});
