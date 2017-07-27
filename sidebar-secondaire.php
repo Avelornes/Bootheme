5dc82859791408b7f984091fa3901fd587263c13
Séverine Noël <severine.noel94@gmail.com>
2017-07-27 22:50:18 +0200

    create new files for sidebars

---
+++ sidebar-secondaire.php
@@ -0,0 +1,7 @@
+<div id="sidebar-primary" class="sidebar">
+	<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
+		<?php dynamic_sidebar( 'sidebar-2' ); ?>
+	<?php else : ?>
+        <!-- Free space for widgets -->
+	<?php endif; ?>
+</div>
